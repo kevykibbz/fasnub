d1a8557c1f878c6eed479bf810853d9864fc44a0
Kevin Kibebe <56401333+kevykibbz@users.noreply.github.com>
2023-12-05 19:11:01 +0300

    Add files via upload

---
+++ resources/lang/en/seo.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| SEO Language Lines
+	|--------------------------------------------------------------------------
+	|
+	*/
+	"slogan" => "Support Content Creators via Membership", // New on v4.0
+	"description" => "Fansnub: Exclusive content hub connecting fans with creators for diverse, engaging videos, photos, write ups, articles and live streams. Join now!",
+	"keywords" => "Fansnub, Fansnub.com,buzz,xaxino club,earning,donations,support,creators,xaxino,subscription,content,donations,support,creators,sponzy,subscription,content",
+);
---
+++ resources/lang/es/seo.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| SEO Language Lines
+	|--------------------------------------------------------------------------
+	|
+	*/
+	"slogan" => "Apoye a los creadores de contenido a través de una membresía", // New on v4.0
+	"description" => "Fansnub: centro de contenido exclusivo que conecta a los fanáticos con creadores para videos, fotos y transmisiones en vivo diversos y atractivos. ¡Únete ahora!",
+	"keywords" => "Fansnub, Fansnub.com, buzz, xaxino club, ganancias, donaciones, apoyo, creadores, xaxino, suscripción, contenido, donaciones, apoyo, creadores, sponzy, suscripción, contenido",
+);
