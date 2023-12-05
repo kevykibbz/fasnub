72b65b220499234ac90073f1f69aec87d4e53ebe
Kevin Kibebe <56401333+kevykibbz@users.noreply.github.com>
2023-12-05 19:05:00 +0300

    Add files via upload

---
+++ app/Models/LoginSessions.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LoginSessions extends Model
+{
+    public $fillable = [
+        'user_id',
+        'ip',
+        'device',
+        'device_type',
+        'browser',
+        'platform',
+        'country',
+        'updated_at'
+    ];
+}
---
+++ app/Models/Subscriptions.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\AdminSettings;
+use App\Models\User;
+use App\Models\Notifications;
+use Mail;
+
+class Subscriptions extends Model
+{
+	protected $guarded = [];
+
+	public function subscriber()
+	{
+		return $this->belongsTo(User::class, 'user_id');
+	}
+
+	public function creator()
+	{
+		return $this->belongsToMany(
+			User::class,
+			Plans::class,
+			'name',
+			'user_id',
+			'stripe_price',
+			'id'
+		);
+	}
+
+	public function user()
+	{
+		return $this->belongsTo(User::class)->first();
+	}
+
+	public function subscribed()
+	{
+		return $this->belongsToMany(
+					User::class,
+					Plans::class,
+					'name',
+					'user_id',
+					'stripe_price',
+					'id'
+				)->first();
+	}
+
+	public static function sendEmailAndNotify($subscriber, $user)
+	{
+		$user = User::select([
+			'id', 
+			'language', 
+			'email', 
+			'name', 
+			'email_new_subscriber', 
+			'notify_new_subscriber'
+			])->whereId($user)->first();
+
+		// Set Lang user
+		app()->setLocale($user->language);
+
+		$titleSite    = config('settings.title');
+		$sender       = config('settings.email_no_reply');
+		$emailUser    = $user->email;
+		$fullNameUser = $user->name;
+		$subject      = $subscriber.' '.__('users.has_subscribed');
+		
+		try {
+			if ($user->email_new_subscriber == 'yes') {				
+			//<------ Send Email to User ---------->>>
+			Mail::send('emails.new_subscriber', [
+				'body' => $subject,
+				'title_site' => $titleSite,
+				'fullname' => $fullNameUser
+			],
+				function($message) use ($sender, $subject, $fullNameUser, $titleSite, $emailUser)
+					{
+					$message->from($sender, $titleSite)
+						->to($emailUser, $fullNameUser)
+						->subject($subject.' - '.$titleSite);
+					});
+				//<------ End Send Email to User ---------->>>
+			}
+
+		} catch (\Exception $e) {
+			\Log::info('Error send email new Subscriber ---'. $e->getMessage());
+		}
+		
+		if ($user->notify_new_subscriber == 'yes') {
+			// Send Notification to User --- destination, author, type, target
+			Notifications::send($user->id, auth()->id(), '1', $user->id);
+		}
+	}
+
+}
